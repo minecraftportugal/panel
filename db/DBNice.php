a73472b735a8b0896774a870d7925993851f62d6
Rui Pedro Lima <ruipedro.lima@gmail.com>
2013-09-07 15:42:49 +0100

    Reestruturei uma beca :-p Fica tudo nos innards, alguns coisos pra fechar fixe a ligação e o fetch. Milhões de opçõe se possibilidades.

--- db/DBNice.php
+++ db/DBNice.php
@@ -3,6 +3,7 @@
 namespace minecraftia\db;
 
 use PDO;
+use PDOStatment;
 use PDOException;
 
 class DBNice {
@@ -13,8 +14,15 @@ class DBNice {
 
   private $_connected = false;
 
+  private $_statement = null;
+
   private $_result = null;
 
+
+  /**
+   * Did anyone called for a constructor?
+   * @param array $config configs to override the defaults
+   */
   public function __construct(array $config = []) {
     $defaults = [
       'prefix' => 'mysql',
@@ -29,6 +37,16 @@ class DBNice {
     ];
     $config += $defaults;
     $this->_config = $config + $defaults;
+
+    $this->connect();
+  }
+
+  public function connect(array $options = []) {
+    if ($this->_connected) {
+      return true;
+    }
+
+    $this->_config += $options;
 
     $dsn = "mysql:host=%s;port=%s;dbname=%s";
     $this->_config['dsn'] = sprintf(
@@ -57,48 +75,60 @@ class DBNice {
     $this->_connected = true;
   }
 
+  /**
+   * Clear objects to ensure memory gets wiped off and the connection is closed
+   * @return void
+   */
   public function close() {
+    unset($this->_result);
+    $this->_result = null;
+    unset($this->_statement);
+    $this->_statement = null;
     unset($this->connection);
     $this->connection = null;
     return $this->_conected = false;
   }
 
 
+  /**
+   * Helper to check how the connection is doing.
+   * @return boolean haz connection?
+   */
   public function isConnected() {
     return $this->_connected;
   }
 
 
-  public function query($sql, $conditions = array(), array $options = array()) {
+  private function _execute($sql, $conditions = [], array $options = []) {
     if (!$this->isConnected()) {
       return false;
     }
 
     try {
-      $stmt = $this->connection->prepare($sql);
+      $this->_statement = $this->connection->prepare($sql);
     } catch (PDOException $e) {
-      die('Invalid SQL statement.');
+      die('Invalid SQL statement: ' . $e->getMessage());
     }
 
     if (!empty($conditions)) {
-      $this->_buildStatement($stmt, $conditions);
+      $this->_buildStatement($conditions);
     }
 
     try {
-      $this->_result = $stmt->execute();
+      $result = $this->_statement->execute();
     } catch (PDOException $e) {
-      die('Error executing query ' . $sql);
+      die('Error executing: ' . $e->getMessage());
     }
 
-    /**
-     * needs to build the results now
-     *
-     * var_dump($stmt->fetchAll(PDO::FETCH_ASSOC)); die;
-     */
+    return $result;
   }
 
-
-  protected function _buildStatement(&$stmt, array $conditions = array()) {
+  /**
+   * Helper for the DBNice::query to clean up the code.
+   * @param  array  $conditions Array of conditions to connect to the query.
+   * @return void
+   */
+  protected function _buildStatement(array $conditions = []) {
     foreach ($conditions as $key => $value) {
       switch ($value) {
         case is_bool($value):
@@ -118,15 +148,54 @@ class DBNice {
       if (is_numeric($key)) {
         $x = $key + 1;
       } else {
-        $x = ':' . $key;
+        $x = ":{$key}";
       }
 
-      $stmt->bindValue($x, $value, $pdoconst);
+      $this->_statement->bindValue($x, $value, $pdoconst);
     }
   }
 
+  /**
+   * Queries the db for inserts, updates and shit like that.
+   * @param  string $sql        A query
+   * @param  array  $conditions The query conditions
+   * @param  array  $options    Misc options (important for fetching stuff)
+   * @return mixed
+   */
+  public function query($sql, $conditions = [], array $options = []) {
+    return $this->_execute($sql, $conditions, $options);
+  }
 
-  protected function getResults() {}
+  /**
+   * Fetches shit, a select query i'd say.
+   * @param  string $sql        The query
+   * @param  array $conditions  The conditions to the query
+   * @param  array $options     Misc options
+   * @return mixed              Array with the expected results or boolean
+   */
+  public function fetch($sql, $conditions = [], array $options = []) {
+    if (!$this->_execute($sql, $conditions, $options)) {
+      return false;
+    }
+    return $this->_getResults($options);
+  }
+
+
+  protected function _getResults(array $options = []) {
+    $defaults = [
+      'type' => PDO::FETCH_ASSOC,
+      'clear' => true
+    ];
+    $options += $defaults;
+
+    $this->_result = $this->_statement->fetchAll($options['type']);
+
+    if (!is_array($this->_result)) {
+      return false;
+    }
+
+    return count($this->_result) === 1 ? $this->_result[0] : $this->_result;
+  }
 }
 
 ?>
--- db/DBNice_test.php
+++ db/DBNice_test.php
@@ -10,9 +10,14 @@ $test = new DBNice();
 
 var_dump('is connected?', $test->isConnected());
 
-// var_dump($test->query('SELECT * FROM accounts'));
+var_dump($test->fetch('SELECT * FROM accounts'));
 
-var_dump($test->query('SELECT * FROM accounts WHERE name = ?', array('wikipeixoto')));
+var_dump($test->fetch('SELECT * FROM accounts WHERE playername = ?', ['wikipeixoto']));
+
+var_dump($test->query(
+  "UPDATE accounts SET active = 0 WHERE id = :id AND active = 1",
+  ['id' => 351]
+));
 
 $test->close();
 
