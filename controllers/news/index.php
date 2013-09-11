d63344e8ab86e0893ca12358c840ab66c13fce2c
Hugo Peixoto <hugo.peixoto@gmail.com>
2013-09-11 21:07:47 +0100

    Adds missing global

--- controllers/news/index.php
+++ controllers/news/index.php
@@ -4,8 +4,11 @@ require_once('lib.php');
 require_once('i18n.php');
 
 function news_index() {
+  global $cfg_wp_enabled;
+
   validateSession();
   refreshxAuthSession($_SESSION['id']);
+
   require('templates/news/index.php');
 }
 
