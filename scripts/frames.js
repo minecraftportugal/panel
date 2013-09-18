081a5cd99aa8cd2aba612a8317115d334108ad1f
dubaiss <spam.me.slowly@gmail.com>
2013-09-18 02:47:15 +0000

    avoiding race condition on webkit fix

--- scripts/frames.js
+++ scripts/frames.js
@@ -6,28 +6,36 @@ if (window.self == window.top) {
 
 $(document).ready(function() {
   $('span.stevehead > img.pixels')
-    .on('error', function(){
-      $(this).attr('src', '/images/steve.png');
-    })
-    .on('load', function(){
-      // webkit anti image smothing hack
-      if (navigator.userAgent.indexOf("AppleWebKit") == -1)
-        return;
-
-      var c = document.createElement('canvas');
-      $(c).addClass('pixels');
-      var w = c.width = this.width;
-      var h = c.height = this.height;
-      $(this).replaceWith(c);
-      var ctx = c.getContext('2d');
-      ctx.imageSmoothingEnabled = false;
-      ctx.mozImageSmoothingEnabled = false;
-      ctx.oImageSmoothingEnabled = false;
-      ctx.webkitImageSmoothingEnabled = false;
-      ctx.drawImage(this, 0, 0, w, h);
-    })
     .attr('src', function(){
-      return $(this).attr('data-src');
+      var ds = $(this).attr('data-src');
+      var defimg = $(this).attr('src');
+
+      // try to prevent load events and src changing simultaneously
+      $(this)
+        .attr('src', '')
+        .one('error', function(){
+          $(this).attr('src', defimg);
+        })
+        .on('load', function(){
+          if (navigator.userAgent.indexOf("AppleWebKit") == -1)
+            return;
+
+          // webkit anti image smoothing hack
+          var c = document.createElement('canvas');
+          $(c).addClass('pixels');
+          var w = c.width = this.width;
+          var h = c.height = this.height;
+          $(this).replaceWith(c);
+
+          var ctx = c.getContext('2d');
+          ctx.imageSmoothingEnabled = false;
+          ctx.mozImageSmoothingEnabled = false;
+          ctx.oImageSmoothingEnabled = false;
+          ctx.webkitImageSmoothingEnabled = false;
+          ctx.drawImage(this, 0, 0, w, h);
+        });
+
+      return ds;
     });
 });
 
