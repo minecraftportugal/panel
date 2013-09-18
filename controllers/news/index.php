7055e85d6f330d049e03e8297fe856be69b14ce4
ticklemynausea <emailoftheyear@gmail.com>
2013-09-19 00:11:08 +0100

    Fixed wp derp

--- controllers/news/index.php
+++ controllers/news/index.php
@@ -5,7 +5,7 @@ require_once('lib/i18n.php');
 require_once('lib/users.php');
 
 function news_index() {
-  global $cfg_wp_enabled;
+  global $cfg_wp_enabled, $cfg_wp_location, $cfg_wp_url;
 
   validateSession();
   refreshxAuthSession($_SESSION['id']);
