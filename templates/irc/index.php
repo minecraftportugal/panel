282c284632d68df768865dc4439748de44ebdaf4
ticklemynausea <emailoftheyear@gmail.com>
2013-09-28 14:19:37 +0100

    Changed script load order (had a $ missing error)

--- templates/irc/index.php
+++ templates/irc/index.php
@@ -7,8 +7,8 @@
  <title></title>
  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
  <script type="text/javascript" src="/scripts/irc.js"></script>
- <script type="text/javascript" src="/scripts/frames.js"></script>
  <script type="text/javascript" src="/scripts/jquery.js"></script>
+ <script type="text/javascript" src="/scripts/frames.js"></script>
  <style type="text/css">
     html { height: 100%; overflow: hidden; }
     body { 
