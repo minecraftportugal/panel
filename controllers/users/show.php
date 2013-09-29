613e2e6bc698a67e106788a251f29ba8f2b2eccd
dubaiss <spam.me.slowly@gmail.com>
2013-09-29 00:22:01 +0000

    enchanted items on inventory, some cleanup

--- controllers/users/show.php
+++ controllers/users/show.php
@@ -12,8 +12,42 @@ function users_show () {
 
   $profile = getUserById($profileId);
   $inquisitor = getInquisitor($profile['playername']);
+
+  // prepare inquisitor data
   if ($inquisitor)
+  {
     $inventory = json_decode($inquisitor['inventory']);
+
+    $playerinv = array();
+
+    foreach($inventory as $slot)
+    { 
+      if ($slot) {
+        $itemdata = "".$slot->type;
+        $itemdata .= " ".$slot->data;
+        $itemdata .= " ".$slot->amount;
+        $itemdata .= " ".$slot->durability;
+
+        $enchantments = array();
+        foreach($slot->enchantments as $name => $level)
+        {
+          array_push($enchantments, "$name".":".$level);
+        }
+        $enchantments = implode(" ", $enchantments);
+        array_push($playerinv, array(
+          "itemdata" => $itemdata,
+          "enchantments" => $enchantments
+        ));
+      }
+      else
+      {
+        array_push($playerinv, array(
+          "itemdata" => "",
+          "enchantments" => ""
+        ));
+      }
+    }
+  }
 
   $userSkin = "http://s3.amazonaws.com/MinecraftSkins/".$_SESSION['username'].".png";
   $profileSkin = "http://s3.amazonaws.com/MinecraftSkins/".$profile['playername'].".png";
