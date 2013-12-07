e770899c3bc506ed6e5272ef2a589f391b26e0c2
Mário Carneiro <emailoftheyear@gmail.com>
2013-12-07 17:35:20 +0000

    More changes

--- scripts/sop-inject.js
+++ scripts/sop-inject.js
@@ -3,6 +3,6 @@ document.domain = "minecraft.pt";
 
 // hide dynmap elements
 $(window.top.document).find("iframe#map").load(function() {
-	this.contentWindow.$('head').append('<link rel="stylesheet" type="text/css" href="//dev.minecraft.pt/styles/dynmap-patch.css">');
+	this.contentWindow.$('head').append('<link rel="stylesheet" type="text/css" href="//www.minecraft.pt/styles/dynmap-patch.css">');
 });
 
