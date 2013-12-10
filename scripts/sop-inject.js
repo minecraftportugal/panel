e438b48de776c06c2e81bdef9e1a6ee4831ba08d
ticklemynausea <emailoftheyear@gmail.com>
2013-12-10 01:52:49 +0000

    Omg testing shit on master

--- scripts/sop-inject.js
+++ scripts/sop-inject.js
@@ -6,3 +6,5 @@ $(window.top.document).find("iframe#map").load(function() {
 	this.contentWindow.$('head').append('<link rel="stylesheet" type="text/css" href="//www.minecraft.pt/styles/dynmap-patch.css">');
 });
 
+// make sure it loads properly inside the panel site
+console.log(parent.document.getElementById(window.name));
