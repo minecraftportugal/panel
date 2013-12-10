6de0fd21aab1e484bef19878946dc1272fbdb41f
Mário Carneiro <emailoftheyear@gmail.com>
2013-12-10 02:03:53 +0000

    Omg dynmap

--- scripts/sop-inject.js
+++ scripts/sop-inject.js
@@ -3,8 +3,10 @@ document.domain = "minecraft.pt";
 
 // hide dynmap elements
 $(window.top.document).find("iframe#map").load(function() {
-	this.contentWindow.$('head').append('<link rel="stylesheet" type="text/css" href="//www.minecraft.pt/styles/dynmap-patch.css">');
+    this.contentWindow.$('head').append('<link rel="stylesheet" type="text/css" href="//www.minecraft.pt/styles/dynmap-patch.css">');
 });
 
 // make sure it loads properly inside the panel site
-console.log(parent.document.getElementById(window.name));
+if (window.top.document == window.document) {
+    window.location = '//www.minecraft.pt/forbidden'
+}
