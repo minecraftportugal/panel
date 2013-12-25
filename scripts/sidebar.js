c019c7eaab31c69a7e7da6fc9b59071c70e55b5c
ticklemynausea <emailoftheyear@gmail.com>
2013-12-25 01:45:10 +0000

    Improved accordion
    Now you can click to close the selected pane

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -4,10 +4,23 @@ $(document).ready(function() {
     $("div.section.default h1").click();
   }
 
+  $("a[href*='#']").click(function(e) {
+    var target = $(this).attr("href");
+    var current = window.location.hash;
+    console.log(target, current);
+    if (target == current) {
+      console.log("AWMG");
+      window.location.hash = "";
+      e.preventDefault();
+    }
+  });
+  
   // Logout
   $("a#logout").click(function(e) {
     top.logout();
     return false;
   });
+
+
 
 });
