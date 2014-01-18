304b4c28bc6616c15cc6734690e539ffbfc59b69
Mário Carneiro <emailoftheyear@gmail.com>
2014-01-18 02:04:02 +0000

    Changed http:// to // in links

--- templates/widgets/players.php
+++ templates/widgets/players.php
@@ -7,7 +7,7 @@
   <title></title>
   <script type="text/javascript" src="/scripts/jquery.js"></script>
   <script type="text/javascript" src="/scripts/steve.js"></script>
-  <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" media="screen" href="/styles/reset.css" />
   <link rel="stylesheet" type="text/css" media="screen" href="/styles/widget_players.css" />
 </head>
@@ -31,6 +31,6 @@
 <? endif;?>
 <p>Servidor da Comunidade Minecraft Portugal</p>
 <p>IP: minecraft.pt</p>
-<p><a href="http://www.minecraft.pt" target="_target">Regista-te já!</a></p>
+<p><a href="//www.minecraft.pt" target="_target">Regista-te já!</a></p>
 </body>
 </html>
