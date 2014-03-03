29a6830bdfd2a886090785632384c5e056d28484
ticklemynausea <emailoftheyear@gmail.com>
2014-03-03 19:18:51 +0000

    Fixed a missing script tag

--- templates/maps/index.php
+++ templates/maps/index.php
@@ -10,8 +10,9 @@
 
     <script type="text/javascript" src="/scripts/jquery.js"></script>
     <script type="text/javascript" src="/scripts/frames.js"></script>
-    <script type="text/javascript" src="/scripts/autoreload.js"></script>
+    <script type="text/javascript" src="/scripts/steve.js"></script>
     <script type="text/javascript" src="/scripts/sidebar.js"></script>
+    <script type="text/javascript" src="/scripts/autoreload.js"></script>
     <script type="text/javascript" src="/scripts/dynmap.js"></script>
     <script type="text/javascript" src="/scripts/sop.js"></script>
 
