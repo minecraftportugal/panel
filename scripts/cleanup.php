089b7790580d4d3cd30d6156f10bb9f895389b01
ticklemynausea <emailoftheyear@gmail.com>
2014-07-22 11:49:07 +0100

    Added cleanup script

---
+++ scripts/cleanup.php
@@ -0,0 +1,68 @@
+ <?
+
+#
+# Minecraftia! Clean up old registrations and crap
+# v1.0
+#
+
+require 'bootstrap.php';
+
+use minecraftia\db\Bitch;
+
+#
+# Apagar todos os itemdrops com mais de um mês
+#
+$sql['old_itemdrops'] = "
+  DELETE FROM itemdrops WHERE dropdate < NOW() - INTERVAL 1 MONTH;
+";
+
+#
+# Apagar todos os registos não usados com mais de 24h 
+#
+$sql['unused_registrations'] = "
+  DELETE FROM accounts WHERE lastlogindate IS NULL AND registerdate < NOW() - INTERVAL 1 DAY;
+";
+
+#
+# Apagar todos os registos usados, mas inactivos há mais de de dois meses e que não jogaram pelo menos 10 horas
+#
+$sql['inactive_registrations'] = "
+  DELETE FROM accounts
+  WHERE lastlogindate < NOW() - INTERVAL 2 MONTH
+  AND playername in (
+    SELECT name
+    FROM inquisitor.players ip
+    WHERE (ip.totalTime <= 10*3600 OR ip.totalTime IS NULL)
+  );    
+";
+
+#
+# Apagar todos os registos do inquisitor que nao estao na tabela accounts
+#
+$sql['inquisitor_accounts'] = "
+  DELETE FROM inquisitor.players
+  WHERE name not IN (
+    SELECT playername FROM accounts
+  );
+";
+
+function do_delete($sql) {
+  
+  global $drop_definitions;
+
+  $start = microtime(true);
+
+  $result = Bitch::source('default')->query($sql);
+
+  $end = microtime(true);
+ 
+  $runtime = round($end - $start, 6) * 1000;
+  $runtime = str_pad($runtime, 8, STR_PAD_LEFT);
+
+  print "done ($runtime" . "ms)\n";
+}
+
+foreach ($sql as $k => $v) {
+    print "Running '$k'...\t\t";
+    do_delete($v);
+}
