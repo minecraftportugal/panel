998f0787a71dd680be321b0351fbf2cbf590a67e
ticklemynausea <emailoftheyear@gmail.com>
2015-06-12 09:26:21 +0100

    scss files and build process

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,27 @@ var uglifycss = require('gulp-uglifycss');
 var minify = require('gulp-minify');
 var concat = require('gulp-concat');
 var symlink = require('gulp-sym');
+var sass = require('gulp-sass');
 
-gulp.task('default', ['js', 'json', 'css', 'fonts', 'images', 'sounds', 'production'], function() {
+/* default sass task */
+gulp.task('default', ['dev-sass', 'build-js', 'build-json', 'build-css', 'build-fonts', 'build-images', 'build-sounds', 'production'], function() {
     // place code for your default task here
-
 });
 
-gulp.task('js', function() {
+/* generate css files from scss files */
+gulp.task('dev-sass', function () {
+    return gulp.src('./assets/development/scss/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/development/css'));
+});
+
+/* watch for the task above */
+gulp.task('dev-sass:watch', function () {
+    gulp.watch('./assets/development/scss/**/*.scss', ['dev-sass']);
+});
+
+/* generate js files for production */
+gulp.task('build-js', function() {
     return gulp.src('./assets/development/js/**/*.js')
         .pipe(uglify({mangle: true}))
         //.pipe(concat('all.js'))
@@ -20,13 +34,15 @@ gulp.task('js', function() {
         .pipe(gulp.dest('./assets/production/js'))
 });
 
-gulp.task('json', function() {
+/* generate json files for production */
+gulp.task('build-json', function() {
     return gulp.src('./assets/development/js/**/*.json')
         .pipe(size())
         .pipe(gulp.dest('./assets/production/js'))
 });
 
-gulp.task('css', function() {
+/* generate css files for production */
+gulp.task('build-css', function() {
     return gulp.src('./assets/development/css/**/*.css')
         .pipe(uglifycss())
         //.pipe(concat('all.js'))
@@ -34,27 +50,32 @@ gulp.task('css', function() {
         .pipe(gulp.dest('./assets/production/css'))
 });
 
-gulp.task('fonts', function() {
-    gulp.src('./assets/development/fonts/**/*')
+/* generate fonts files for production */
+gulp.task('build-fonts', function() {
+    return gulp.src('./assets/development/fonts/**/*')
         .pipe(gulp.dest('./assets/production/fonts'))
 });
 
-gulp.task('images', function() {
-    gulp.src('./assets/development/images/**/*')
+/* generate images files for production */
+gulp.task('build-images', function() {
+    return gulp.src('./assets/development/images/**/*')
         .pipe(gulp.dest('./assets/production/images'))
 });
 
-gulp.task('sounds', function() {
-    gulp.src('./assets/development/sounds/**/*')
+/* generate sounds files for production */
+gulp.task('build-sounds', function() {
+    return gulp.src('./assets/development/sounds/**/*')
         .pipe(gulp.dest('./assets/production/sounds'))
 });
 
+/* use the development environment */
 gulp.task('development', function() {
-    gulp.src('./assets/development')
+    return gulp.src('./assets/development')
         .pipe(symlink('./public', {force: true, relative: true}))
 });
 
+/* use the production environment */
 gulp.task('production', function() {
-    gulp.src('./assets/production')
+    return gulp.src('./assets/production')
         .pipe(symlink('./public', {force: true, relative: true}))
 });
