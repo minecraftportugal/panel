350c052b7ac16d19e08e502a9f65dc98c47c2c6a
ticklemynausea <emailoftheyear@gmail.com>
2015-06-27 12:42:21 +0100

    show loading blocker when changing backgrounds

--- assets/development/js/forms.js
+++ assets/development/js/forms.js
@@ -10,6 +10,9 @@ App.Forms.userOptions = function() {
     $("form[name=bg_settings]").submit(function(e) {
         e.preventDefault();
 
+        var loading_blocker = $(this).closest("div.body").next();
+        loading_blocker.addClass("block-enabled");
+
         App.Desktop.settings.sounds = $("input#chk_sounds").prop("checked");
 
         var imageURL = $("input#input_url").val();
@@ -33,6 +36,10 @@ App.Forms.userOptions = function() {
                 "duration": 3000,
                 "sound" : "break"
             });
+        });
+
+        deferred.always(function() {
+            loading_blocker.removeClass("block-enabled");
         });
 
         return false;
