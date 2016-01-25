1518caf4fa88c482ab3ec9f5f05d1c89581ce2bd
Mário C <igotbugs@ticklemynausea.net>
2016-01-25 13:08:45 +0000

    synchronous ad code

--- templates/partials/ads/leaderboard-bottom.php
+++ templates/partials/ads/leaderboard-bottom.php
@@ -1,10 +1,13 @@
 <a id="ad-donate" href="//www.mcpt.eu/contribuir" target="_blank">A sobrevivencia do mcpt.eu está em causa! Remove os ads do site com um donativo e ajuda-nos a suportar os custos mensais!</a>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- panel.mcpt.eu_leaderboard_bottom -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px;margin-top:3px;"
-     data-ad-client="ca-pub-4464482136128973"
-     data-ad-slot="7162572334"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+<div style="display:inline-block;width:728px;height:90px;margin-top:3px;">
+<script type="text/javascript">
+    google_ad_client = "ca-pub-4464482136128973";
+    google_ad_slot = "7162572334";
+    google_ad_width = 728;
+    google_ad_height = 90;
 </script>
+<!-- panel.mcpt.eu_leaderboard_bottom -->
+<script type="text/javascript"
+src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
